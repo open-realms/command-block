b87904a55954983c1c96cbfa1df2289aff88cabb
Andrew Woodruff <44043065+andrewgwoodruff@users.noreply.github.com>
2021-04-15 15:41:23 -0600

    Update src/droplet/droplet.service.ts
    Co-authored-by: John Woodruff <5883616+johnbwoodruff@users.noreply.github.com>

--- src/droplet/droplet.service.ts
+++ src/droplet/droplet.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DropletService {
-  getAllDroplets(): string {
+  async getAllDroplets(): string {
     return 'Here are all the servers!';
   }
 
