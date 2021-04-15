7d7a0dd03a16e49062d05ad90532fc032bbfea92
Andrew Woodruff <agwoodruff93@gmail.com>
2021-04-15 11:49:36 -0600

    block out initial code for the various endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('servers')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllServers(): string {
+    return this.appService.getAllServers();
+  }
+
+  @Post()
+  createServer(): string {
+    return this.appService.createServer();
+  }
+
+  @Delete(':id')
+  deleteServer(@Param() params): string {
+    return this.appService.deleteServer(params.id);
+  }
+
+  @Get(':id')
+  getServerDetails(@Param() params): string {
+    return this.appService.getServerDetails(params.id);
+  }
+
+  @Put(':id')
+  updateServerDetails(@Param() params): string {
+    return this.appService.updateServerDetails(params.id);
+  }
+
+  @Put(':id/start')
+  startServer(@Param() params): string {
+    return this.appService.startServer(params.id);
+  }
+
+  @Put(':id/stop')
+  stopServer(@Param() params): string {
+    return this.appService.stopServer(params.id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,31 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllServers(): string {
+    return 'Here are all the servers!';
+  }
+
+  createServer(): string {
+    return 'A new server has been created!';
+  }
+
+  deleteServer(id: string): string {
+    return 'Server ${id} has been deleted!';
+  }
+
+  getServerDetails(id: string): string {
+    return 'Here are the details for this server: ${id}';
+  }
+
+  updateServerDetails(id: string): string {
+    return 'Server ${id} has been updated!';
+  }
+
+  startServer(id: string): string {
+    return 'Server ${id} has been started!';
+  }
+
+  stopServer(id: string): string {
+    return 'Server ${id} has been stopped!';
   }
 }
