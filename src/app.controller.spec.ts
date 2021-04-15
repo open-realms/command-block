5ee4b36e0efd7810d5848ec94d6335b87ddb1c9b
Andrew Woodruff <agwoodruff93@gmail.com>
2021-04-15 12:01:03 -0600

    change names of files to refer to droplet

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.createDroplet()).toBe('A new server has been created!');
     });
   });
 });
