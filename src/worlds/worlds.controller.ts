249cd9d40b6ef07e42dfdc0c174a1866d5ca7bc7
Andrew Woodruff <agwoodruff93@gmail.com>
2021-04-15 19:31:15 -0600

    rename 'worldService' to 'worldsService'

--- src/worlds/worlds.controller.ts
+++ src/worlds/worlds.controller.ts
@@ -3,40 +3,40 @@ import { WorldsService } from './worlds.service';
 
 @Controller('worlds')
 export class WorldsController {
-  constructor(private readonly worldService: WorldsService) {}
+  constructor(private readonly worldsService: WorldsService) {}
 
   @Get()
   async getAllWorlds(): Promise<string> {
-    return await this.worldService.getAllWorlds();
+    return await this.worldsService.getAllWorlds();
   }
 
   @Post()
   async createWorld(): Promise<string> {
-    return await this.worldService.createWorld();
+    return await this.worldsService.createWorld();
   }
 
   @Delete(':id')
   async deleteWorld(@Param() params): Promise<string> {
-    return await this.worldService.deleteWorld(params.id);
+    return await this.worldsService.deleteWorld(params.id);
   }
 
   @Get(':id')
   async getWorldDetails(@Param() params): Promise<string> {
-    return await this.worldService.getWorldDetails(params.id);
+    return await this.worldsService.getWorldDetails(params.id);
   }
 
   @Put(':id')
   async updateWorldDetails(@Param() params): Promise<string> {
-    return await this.worldService.updateWorldDetails(params.id);
+    return await this.worldsService.updateWorldDetails(params.id);
   }
 
   @Put(':id/start')
   async startWorld(@Param() params): Promise<string> {
-    return await this.worldService.startWorld(params.id);
+    return await this.worldsService.startWorld(params.id);
   }
 
   @Put(':id/stop')
   async stopWorld(@Param() params): Promise<string> {
-    return await this.worldService.stopWorld(params.id);
+    return await this.worldsService.stopWorld(params.id);
   }
 }
